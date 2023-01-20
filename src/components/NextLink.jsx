eb419a7c37821a0fd56142d2419ec8556e3d4926
Serge MUGISHA <mugishasergebonheur@gmail.com>
2023-01-20 18:52:41 -0500

    Update NextLink.jsx

--- src/components/NextLink.jsx
+++ src/components/NextLink.jsx
@@ -6,7 +6,6 @@ import ReactGA from 'react-ga';
 export const NextLink = (props) => {
     const sendAnalytics = () => {
         if (props?.href?.includes("http")) {
-            console.log("navigated to ||", props.href, "||")
             ReactGA.event({
                 category: 'Outbound link <<<',
                 action: `Navigated to || ${props?.href} ||`
