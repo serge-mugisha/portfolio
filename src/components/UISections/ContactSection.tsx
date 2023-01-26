36f29c2cc095fbbc02a497968f8de5bf453842be
Serge MUGISHA <mugishasergebonheur@gmail.com>
2023-01-25 22:26:59 -0500

    Test CI/CD

--- src/components/UISections/ContactSection.tsx
+++ src/components/UISections/ContactSection.tsx
@@ -4,7 +4,7 @@ const ContactSection = () => {
     return (
         <div className="py-10">
             <h2 className="h3">Let's connect</h2>
-            <p className="my-3">I'm always open to discuss project design and development work, and partnership opportunities.</p>
+            <p className="my-3">I'm always open to discuss project design, development work, and partnership opportunities.</p>
             <Button isLink classes="text-white bg-dark text-base" text="E-mail: bsergemugisha@gmail.com" href="mailto:bsergemugisha@gmail.com"></Button>
         </div>
     )
